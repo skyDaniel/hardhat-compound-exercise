1c1ac03437caeeaf5c2dfab2e43194b0dcbe13a8
Daniel Hsu <daniel2007704@gmail.com>
2022-11-14 18:38:10 +0800

    fix aave-protocol-v2

--- contracts/aave-protocol-v2/FlashLoanReceiverBase.sol
+++ contracts/aave-protocol-v2/FlashLoanReceiverBase.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.6.12;
+pragma solidity ^0.8.17;
 
-import {SafeMath} from '../../dependencies/openzeppelin/contracts/SafeMath.sol';
-import {IERC20} from '../../dependencies/openzeppelin/contracts/IERC20.sol';
-import {SafeERC20} from '../../dependencies/openzeppelin/contracts/SafeERC20.sol';
-import {IFlashLoanReceiver} from '../interfaces/IFlashLoanReceiver.sol';
-import {ILendingPoolAddressesProvider} from '../../interfaces/ILendingPoolAddressesProvider.sol';
-import {ILendingPool} from '../../interfaces/ILendingPool.sol';
+import {SafeMath} from '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import {IFlashLoanReceiver} from './IFlashLoanReceiver.sol';
+import {ILendingPoolAddressesProvider} from './ILendingPoolAddressesProvider.sol';
+import {ILendingPool} from './ILendingPool.sol';
 
 abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   using SafeERC20 for IERC20;
@@ -15,7 +15,7 @@ abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
   ILendingPoolAddressesProvider public immutable override ADDRESSES_PROVIDER;
   ILendingPool public immutable override LENDING_POOL;
 
-  constructor(ILendingPoolAddressesProvider provider) public {
+  constructor(ILendingPoolAddressesProvider provider) {
     ADDRESSES_PROVIDER = provider;
     LENDING_POOL = ILendingPool(provider.getLendingPool());
   }
